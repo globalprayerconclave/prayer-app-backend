94f05441fa0abb55b021678363d36b3725d0b74c
globalprayerconclave <globalprayerconclave@gmail.com>
2025-08-29 10:25:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const Country = require('./models/Country');
+const PrayerScheduleSlot = require('./models/PrayerScheduleSlot');
 
 const app = express();
 
